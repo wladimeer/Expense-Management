58397a1778bd729d9210dc34fbd43c6c28ee0c21
unknown <sbenavidescartes@gmail.com>
2023-05-17 16:38:31 -0400

    Guard implementation and corrections applied

--- src/pages/private/NewExpense.js
+++ src/pages/private/NewExpense.js
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { HeaderButton, Headline } from '../../elements/Header';
 import { Header, HeaderContent, HeaderButtonGroup } from '../../elements/Header';
-// import { AuthContext } from '../../contexts/AuthContext';
 import ExitButton from '../../components/ExitButton';
 import { Helmet } from 'react-helmet-async';
 
 const NewExpense = () => {
-  // const { user } = useContext(AuthContext);
-
   return (
     <>
       <Helmet>
@@ -20,7 +17,7 @@ const NewExpense = () => {
 
           <HeaderButtonGroup>
             <HeaderButton to='/categories'>Categories</HeaderButton>
-            <HeaderButton to='/expense-list'>Expense List</HeaderButton>
+            <HeaderButton to='/expense/list'>Expense List</HeaderButton>
             <ExitButton />
           </HeaderButtonGroup>
         </HeaderContent>
--- src/router/General.js
+++ src/router/General.js
@@ -5,17 +5,21 @@ import ModifyExpense from '../pages/private/ModifyExpense';
 import NewExpense from '../pages/private/NewExpense';
 import SignUp from '../pages/public/SignUp';
 import SignIn from '../pages/public/SignIn';
+import PrivateGuard from './PrivateGuard';
+import PublicGuard from './PublicGuard';
 
 const General = () => {
   return (
     <Routes>
-      <Route path='/' element={<NewExpense />} />
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/categories' element={<Categories />} />
-      <Route path='/modify/:expenseId' element={<ModifyExpense />} />
-      <Route path='/expense-list' element={<ExpenseList />} />
-      <Route path='*' element={<NewExpense />} />
+      <Route path='/' element={<PrivateGuard><NewExpense /></PrivateGuard>} />
+      <Route path='/categories' element={<PrivateGuard><Categories /></PrivateGuard>} />
+      <Route path='/expense/modify/:expenseId' element={<PrivateGuard><ModifyExpense /></PrivateGuard>} />
+      <Route path='/expense/list' element={<PrivateGuard><ExpenseList /></PrivateGuard>} />
+
+      <Route path='/sign-in' element={<PublicGuard><SignIn /></PublicGuard>} />
+      <Route path='/sign-up' element={<PublicGuard><SignUp /></PublicGuard>} />
+
+      <Route path='*' element={<PrivateGuard><NewExpense /></PrivateGuard>} />
     </Routes>
   );
 }
